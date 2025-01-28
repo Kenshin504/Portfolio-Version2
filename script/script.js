788ba51af96bf2a327af93aaf4ced361f67212a2
Ryan <ryanlumbis712@gmail.com>
2025-01-28 14:05:37 +0800

    JavaScript Added

---
+++ script/script.js
@@ -0,0 +1,14 @@
+function facebook()
+{
+  window.open('https://www.facebook.com/ryan.lumbis.3/');
+}
+
+function gmail()
+{
+  window.open('mailto:ryanlumbis712@gmail.com');
+}
+
+function github()
+{
+  window.open('https://github.com/Kenshin504');
+}
